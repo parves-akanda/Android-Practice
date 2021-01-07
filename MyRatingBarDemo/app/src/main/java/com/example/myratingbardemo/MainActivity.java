da7cf4b599a5e66299cc52a0ee4827e9771a0751
parves <parves0316@gmail.com>
2021-01-07 16:31:55 +0600

    complete RatingBar Work

--- MyRatingBarDemo/app/src/main/java/com/example/myratingbardemo/MainActivity.java
+++ MyRatingBarDemo/app/src/main/java/com/example/myratingbardemo/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myratingbardemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RatingBar.OnRatingBarChangeListener {
+
+    private RatingBar ratingBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBarId);
+        textView  = findViewById(R.id.textViewId);
+
+        ratingBar.setOnRatingBarChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+        textView.setText("Value : "+rating);
     }
 }
